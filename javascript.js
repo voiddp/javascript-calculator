965c09af31b5e44ae231e7c73036965b9720133a
Ross117 <RossMorran@hotmail.co.uk>
2018-02-26 19:22:15 +0000

    Tidy up JS code

--- javascript.js
+++ javascript.js
@@ -1,126 +1,105 @@
-function attachListeners () {
-  "use strict";
+'use strict';
 
-  function digitBtn (clickEvent) {
+function attachListeners() {
+  const operatorRE = /^([+]|[-]|[x]|[÷])$/g;
 
-    const userInputScreen = document.querySelector(".userInputScreen");
-    const inputArr = userInputScreen.value.split(" ");
+  function digitBtn(clickEvent) {
+    const userInputScreen = document.querySelector('.userInputScreen');
+    const inputArr = userInputScreen.value.split(' ');
     const lastValue = inputArr[inputArr.length - 1];
     const btnValue = clickEvent.currentTarget.textContent;
 
-    if (userInputScreen.value === "0") {
-      userInputScreen.value = "";
+    if (userInputScreen.value === '0') {
+      userInputScreen.value = '';
     }
 
-    //  if the last inputArr item is an operator or a decimal point (i.e. NaN),
-    //  add a space
-    if (isNaN(lastValue)) {
-      userInputScreen.value = userInputScreen.value + " " + btnValue;
+    //  if the last inputArr item is an operator, add a space
+    if (lastValue.match(operatorRE) !== null) {
+      userInputScreen.value += ` ${btnValue}`;
     } else {
-      userInputScreen.value = userInputScreen.value + btnValue;
+      userInputScreen.value += btnValue;
     }
 
     userInputScreen.focus();
   }
 
-  function operatorBtn (clickEvent) {
-
-    const userInputScreen = document.querySelector(".userInputScreen");
-    const inputArr = userInputScreen.value.split(" ");
+  function operatorBtn(clickEvent) {
+    const userInputScreen = document.querySelector('.userInputScreen');
+    const inputArr = userInputScreen.value.split(' ');
     const lastValue = inputArr[inputArr.length - 1];
 
-    if (userInputScreen.value === "" || isNaN(lastValue) || lastValue[lastValue.length -1] === ".") {
+    if (userInputScreen.value === '' || lastValue.match(operatorRE) !== null || lastValue[lastValue.length - 1] === '.') {
       return;
     }
 
-    userInputScreen.value = userInputScreen.value + " " +
-      clickEvent.currentTarget.textContent;
-
+    userInputScreen.value += ` ${clickEvent.currentTarget.textContent}`;
     userInputScreen.focus();
   }
 
-  function decimalPntBtn (clickEvent) {
-
-    const userInputScreen = document.querySelector(".userInputScreen");
-    const inputArr = userInputScreen.value.split(" ");
+  function decimalPntBtn() {
+    const userInputScreen = document.querySelector('.userInputScreen');
+    const inputArr = userInputScreen.value.split(' ');
     const lastValue = inputArr[inputArr.length - 1];
-    const btnValue = clickEvent.currentTarget.textContent;
+    const btnValue = '.';
+    const tailoredRE = /^([+]|[-]|[x]|[÷])$|[.]/g;
 
-    if (userInputScreen.value === "" || isNaN(lastValue) || isNaN(lastValue + btnValue)) {
-      return;
-    }
+    if (userInputScreen.value === '' || lastValue.match(tailoredRE) !== null) return;
 
-    userInputScreen.value = userInputScreen.value +
-      clickEvent.currentTarget.textContent;
-
+    userInputScreen.value += btnValue;
     userInputScreen.focus();
   }
 
-  function calculate () {
-
-    const userInputScreen = document.querySelector(".userInputScreen");
-    let inputArr = userInputScreen.value.split(" ");
+  function calculate() {
+    const userInputScreen = document.querySelector('.userInputScreen');
+    let inputArr = userInputScreen.value.split(' ');
     const lastValue = inputArr[inputArr.length - 1];
-    let result;
     let nxtValue;
 
     // return true if the bodmas rule needs to be implemented
-    function bodmasNeeded (inputArr) {
-
-      if ((inputArr.indexOf("x") !== -1 || inputArr.indexOf("÷") !== -1) &&
-        (inputArr.indexOf("+") !== -1 || inputArr.indexOf("-") !== -1)) {
+    function bodmasNeeded(arr) {
+      if ((arr.indexOf('x') !== -1 || arr.indexOf('÷') !== -1) &&
+        (arr.indexOf('+') !== -1 || arr.indexOf('-') !== -1)) {
         return true;
-      } else {
-        return false;
       }
+      return false;
     }
 
     // handle any precision errors
-    function handlePrecisionErr (val) {
-
-      let cleanedVal;
-
-      cleanedVal = Math.round(val * 1000000000) / 1000000000;
+    function handlePrecisionErr(val) {
+      const cleanedVal = Math.round(val * 1000000000) / 1000000000;
       return cleanedVal;
     }
 
     // implement BODMAS rules
-    function orderOperations (inputArr) {
+    function orderOperations(arr) {
+      const cleanedArr = arr.map(val => val);
 
-      let cleanedArr;
-      const userInputScreen = document.querySelector(".userInputScreen");
-
-      cleanedArr = inputArr.map((val) => {
-        return val;
-      });
-
-      for (let i = 0; i < cleanedArr.length; i++) {
-        let val;
+      for (let i = 0; i < cleanedArr.length; i += 1) {
         let newVal;
         let cleanedVal;
 
         if (!bodmasNeeded(cleanedArr)) return cleanedArr;
 
-        val = cleanedArr[i];
+        const val = cleanedArr[i];
 
-        if (i % 2 !== 0 && (val === "x" || val === "÷")) {
+        if (i % 2 !== 0 && (val === 'x' || val === '÷')) {
         // if the current value is a multiplication or division operator,
         // perform that calcuation, and replace it in the array with the
         // return value - i.e. replace 3 values with a single value
           switch (val) {
-            case "x":
+            case 'x':
               newVal = +cleanedArr[i - 1] * +cleanedArr[i + 1];
               cleanedVal = handlePrecisionErr(newVal);
               cleanedArr.splice(i - 1, 3, cleanedVal);
               break;
-            case "÷":
+            case '÷':
               newVal = +cleanedArr[i - 1] / +cleanedArr[i + 1];
               cleanedVal = handlePrecisionErr(newVal);
               cleanedArr.splice(i - 1, 3, cleanedVal);
               break;
-            default: userInputScreen.value = "Sorry, an error has occured";
+            default: userInputScreen.value = 'Sorry, an error has occured';
           }
-      // restart loop from the beginning of the altered array
+          // restart loop from the beginning of the altered array
           i = -1;
         }
       }
@@ -128,9 +107,7 @@ function attachListeners () {
       return cleanedArr;
     }
 
-    if (isNaN(lastValue) || lastValue === "." || lastValue[lastValue.length -1] === ".") {
-      return;
-    }
+    if (lastValue.match(operatorRE) || lastValue[lastValue.length - 1] === '.') return;
 
     // if the bodmas rule needs to be implemented, call a function which will
     // return an array which conforms to the rule
@@ -138,10 +115,9 @@ function attachListeners () {
       inputArr = orderOperations(inputArr);
     }
 
-  // the odd index numbers will be operators
-  // calculate in groups of 3 (accumulator, operator & val following operator)
-    result = inputArr.reduce( (accumulator, operator, index) => {
-
+    // the odd index numbers will be operators
+    // calculate in groups of 3 (accumulator, operator & val following operator)
+    const result = inputArr.reduce((accumulator, operator, index) => {
       let val;
       let cleanedVal;
 
@@ -152,19 +128,19 @@ function attachListeners () {
       // and calculation can be performed
       if (index % 2 !== 0) {
         switch (operator) {
-          case "+":
+          case '+':
             val = +accumulator + +nxtValue;
             cleanedVal = handlePrecisionErr(val);
             return cleanedVal;
-          case "-":
+          case '-':
             val = +accumulator - +nxtValue;
             cleanedVal = handlePrecisionErr(val);
             return cleanedVal;
-          case "x":
+          case 'x':
             val = +accumulator * +nxtValue;
             cleanedVal = handlePrecisionErr(val);
             return cleanedVal;
-          case "÷":
+          case '÷':
             val = +accumulator / +nxtValue;
             cleanedVal = handlePrecisionErr(val);
             return cleanedVal;
@@ -178,44 +154,41 @@ function attachListeners () {
   }
 
   // clears calculator screen
-  function cancel () {
+  function cancel() {
+    const userInputScreen = document.querySelector('.userInputScreen');
 
-    const userInputScreen = document.querySelector(".userInputScreen");
-
-    userInputScreen.value = "";
+    userInputScreen.value = '';
     userInputScreen.placeholder = 0;
   }
 
   // clears last user entry
-  function clearEntry () {
+  function clearEntry() {
+    const userInputScreen = document.querySelector('.userInputScreen');
+    const inputArr = userInputScreen.value.split(' ');
 
-    const userInputScreen = document.querySelector(".userInputScreen");
-    const inputArr = userInputScreen.value.split(" ");
-
-    if (userInputScreen.value === "") return;
+    if (userInputScreen.value === '') return;
 
     // remove the last element in the array of values entered by the user
     inputArr.splice(inputArr.length - 1, 1);
     // update the calculator screen
-    userInputScreen.value = inputArr.join(" ");
+    userInputScreen.value = inputArr.join(' ');
 
     userInputScreen.focus();
   }
 
   // switch the last value entered on calculator screen to either negative,
   // or positive
-  function convert () {
-
-    const userInputScreen = document.querySelector(".userInputScreen");
-    const inputArr = userInputScreen.value.split(" ");
+  function convert() {
+    const userInputScreen = document.querySelector('.userInputScreen');
+    const inputArr = userInputScreen.value.split(' ');
     let lastValue = inputArr[inputArr.length - 1];
 
-    if (userInputScreen.value === "" || isNaN(lastValue)) return;
+    if (userInputScreen.value === '' || lastValue.match(operatorRE)) return;
 
     // check if last value is neg or pos, change accordingly
     if (lastValue > 0) {
       // add "-"
-      lastValue = "-" + lastValue;
+      lastValue = `-${lastValue}`;
     } else {
       // remove "-"
       lastValue = lastValue.substr(1);
@@ -225,36 +198,36 @@ function attachListeners () {
     // with the newly converted value
     inputArr.splice(inputArr.length - 1, 1, lastValue);
     // update the calculator screen
-    userInputScreen.value = inputArr.join(" ");
+    userInputScreen.value = inputArr.join(' ');
 
     userInputScreen.focus();
   }
 
-  const digitBtns = document.querySelectorAll(".digitBtn");
-  const operatorBtns = document.querySelectorAll(".operatorBtn");
+  const digitBtns = document.querySelectorAll('.digitBtn');
+  const operatorBtns = document.querySelectorAll('.operatorBtn');
 
-  Array.from(digitBtns).forEach( btn => {
-    btn.addEventListener("click", digitBtn);
+  Array.from(digitBtns).forEach((btn) => {
+    btn.addEventListener('click', digitBtn);
   });
 
-  Array.from(operatorBtns).forEach( btn => {
-    btn.addEventListener("click", operatorBtn);
+  Array.from(operatorBtns).forEach((btn) => {
+    btn.addEventListener('click', operatorBtn);
   });
 
-  document.querySelector(".decimalPnt")
-    .addEventListener("click", decimalPntBtn);
+  document.querySelector('.decimalPnt')
+    .addEventListener('click', decimalPntBtn);
 
-  document.querySelector(".calcBtn")
-    .addEventListener("click", calculate);
+  document.querySelector('.calcBtn')
+    .addEventListener('click', calculate);
 
-  document.querySelector(".cancelBtn")
-    .addEventListener("click", cancel);
+  document.querySelector('.cancelBtn')
+    .addEventListener('click', cancel);
 
-  document.querySelector(".clearEntryBtn")
-    .addEventListener("click", clearEntry);
+  document.querySelector('.clearEntryBtn')
+    .addEventListener('click', clearEntry);
 
-  document.querySelector(".converter")
-    .addEventListener("click", convert);
+  document.querySelector('.converter')
+    .addEventListener('click', convert);
 }
 // call attach event listeners function
-attachListeners ();
+attachListeners();
