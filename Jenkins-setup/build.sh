17aa16165d761e50c3ea1d18c956f0de8d9ad1a2
voiddp <voiddp@gmail.com>
2020-06-10 22:04:19 +0300

    edit

--- Jenkins-setup/build.sh
+++ Jenkins-setup/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+indexfile=""
 rm ./Build -vr
 if [ ! -d ./Build ]; then mkdir Build; fi;
 rsync -av --exclude '.git' --exclude 'Jenkins-setup' . ./Build
+mv ./Build/js_calculator.html ./Build/index.html
 
