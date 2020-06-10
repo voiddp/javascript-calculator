609763b63959f33fa656b559188664f25b37c7ac
voiddp <voiddp@gmail.com>
2020-06-10 21:06:26 +0300

    added find\copy to build

--- Jenkins-setup/build.sh
+++ Jenkins-setup/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+builddir="./Build"
+rm $builddir -vr
 if [ ! -d ./Build ]; then mkdir Build; fi;
-ls -l
+find . -not \( -path $builddir -prune \) -not \( -path ./Jenkins-setup -prune \) -not \( -path ./.git -prune \) -exec cp -rv {} ./Build \;
+
