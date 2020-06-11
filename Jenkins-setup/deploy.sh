ca163783b0ab656b98aa70fe602416985c58beb6
voiddp <voiddp@gmail.com>
2020-06-11 03:54:18 +0300

    fix

--- Jenkins-setup/deploy.sh
+++ Jenkins-setup/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "deploying docker image $2:$3 into container $1"
 #removing previous containters
-for container in docker ps -aq --filter "name=$1"
+for container in $(docker ps -aq --filter "name=$1")
 do 
 	docker stop ${container} && docker rm -fv ${container}
 done
 #removing previous images
-for image in docker images -aq "$2:*"
+for image in $(docker images -aq "$2:*")
 do
 	docker rmi -f ${image}
 done
